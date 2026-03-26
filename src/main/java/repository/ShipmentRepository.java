112cc58d8365c29fd08c513de3852dbf2b69055f
Jawaadallie <230472125@mycput.ac.za>
2026-03-27 01:30:46 +0200

    ShipmentRepository.java

--- src/main/java/repository/ShipmentRepository.java
+++ src/main/java/repository/ShipmentRepository.java
@@ -4,5 +4,5 @@ package repository;
 import domain.Shipment;
 
 public interface ShipmentRepository extends IRepository<Shipment, String> {
-    // You can add custom methods here later if needed
+
 }
